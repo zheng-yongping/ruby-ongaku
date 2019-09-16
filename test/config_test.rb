908bec6a645fcce2df248fe7ff83ab26e7c20e55
zheng-yongping <zheng-yongping@hotmail.com>
2019-09-16 22:54:34 +0800

    fix: ConfigTest.test_config change Ongaku::Config.sample_rate

--- test/config_test.rb
+++ test/config_test.rb
@@ -3,8 +3,10 @@ require "test_helper"
 class ConfigTest < Minitest::Test
   def test_config
     _test_config(:sample_rate, 48000)
+    old_sample_rate = Ongaku::Config.sample_rate
     Ongaku::Config.sample_rate = 100
     _test_config(:sample_rate, 100)
+    Ongaku::Config.sample_rate = old_sample_rate
   end
 
   def _test_config(name, expected)
