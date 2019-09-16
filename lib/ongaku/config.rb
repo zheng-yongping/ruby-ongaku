4135d0474c3fbcd95bd4eddc12aa336bbd784d65
zheng-yongping <zheng-yongping@hotmail.com>
2019-09-16 22:47:48 +0800

    refactor: Ongaku::Config

--- lib/ongaku/config.rb
+++ lib/ongaku/config.rb
@@ -2,15 +2,10 @@ require_relative 'helper'
 
 module Ongaku
   module Config
-    extend DefineClassMethod
+    @sample_rate = 48000
 
-    def self.def_config(name, value)
-      class_variable_name = "@@#{name}"
-      class_variable_set(class_variable_name, value)
-      define_class_method(name){class_variable_get(class_variable_name)}
-      define_class_method("#{name}="){|value| class_variable_set(class_variable_name, value)}
+    class << self
+      attr_accessor :sample_rate
     end
-
-    def_config :sample_rate, 48000
   end
 end
